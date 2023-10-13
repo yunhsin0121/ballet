1fe9880bad5e36b541eb52f9962fdd97da4f7f8e
yunhsin <alice334830@gmail.com>
2023-10-14 01:51:30 +0800

    update

--- javascript/js.js
+++ javascript/js.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    // 上滑健
-    $("a[href$='.zip']").addClass('fa-solid fa-circle-up text-light-gray fs-1') ;
+    // 上滑按鈕
+    $("a[href$='.zip']").addClass('bi bi-arrow-up-circle text-secondary fs-1  ') ;
 
     $('.btn-up').click(function (e) { 
         e.preventDefault();
@@ -37,4 +37,67 @@ $(document).ready(function () {
         }
     );
     AOS.init();
+});
+
+// 立即預約選單頁面切換
+let reservationMenu = document.querySelector('.ReservationMenu');
+let reservationMenuArr = document.querySelectorAll('.ReservationMenu a');
+let choosePlan = document.querySelector('.choosePlan');
+let completeInformation = document.querySelector('.completeInformation');
+let completeReservation = document.querySelector('.completeReservation');
+reservationMenu.addEventListener('click',e=>{ 
+    e.preventDefault();
+    reservationMenuArr.forEach(item=>{
+        item.classList.remove('active');
+    })
+    e.target.classList.add('active');
+    if(e.target.dataset.name === 'choosePlan'){
+        choosePlan.classList.add('d-block')
+        choosePlan.classList.remove('d-none')
+        completeInformation.classList.add('d-none')
+        completeInformation.classList.remove('d-block')
+        completeReservation.classList.add('d-none')
+        completeReservation.classList.remove('d-block')
+    }else if(e.target.dataset.name === 'completeInformation'){
+        choosePlan.classList.add('d-none')
+        choosePlan.classList.remove('d-block')
+        completeInformation.classList.add('d-block')
+        completeInformation.classList.remove('d-none')
+        completeReservation.classList.add('d-none')
+        completeReservation.classList.remove('d-block')
+    }else{
+        choosePlan.classList.add('d-none')
+        choosePlan.classList.remove('d-block')
+        completeInformation.classList.add('d-none')
+        completeInformation.classList.remove('d-block')
+        completeReservation.classList.add('d-block')
+        completeReservation.classList.remove('d-none')
+    }
+})
+// 首頁swiper
+var feedbackSwiper = new Swiper(".feedbackSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    cssMode:true,
+    mousewheel: {
+        invert: true,
+    }   ,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        320: {
+        slidesPerView: 1,
+        spaceBetween: 30
+        },
+        768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+        },
+        991: {
+        slidesPerView: 3,
+        spaceBetween: 20
+        },
+    }
 });
